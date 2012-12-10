5078fe62d6a4952fc694e2d39fe5510f05ac4aab
Alexis Gallagher <alexis@alexisgallagher.com>
2012-12-10 10:58:19 +0000

    Initial Commit

---
+++ StripAlphaIfNeeded/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StripAlphaIfNeeded
+//
+//  Created by Alexis Gallagher on 2012-12-10.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
